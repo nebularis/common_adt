42b86135dfa0ddc6eaab77bd6cebae847d58a1b2
Tim Watson <watson.timothy@gmail.com>
2011-11-16 13:16:24 +0000

    Adding rvirding/rb via subtree merge

---
+++ test/stack_props.erl
@@ -0,0 +1,33 @@
+%% -----------------------------------------------------------------------------
+%% Copyright (c) 2002-2011 Tim Watson (watson.timothy@gmail.com)
+%%
+%% Permission is hereby granted, free of charge, to any person obtaining a copy
+%% of this software and associated documentation files (the "Software"), to deal
+%% in the Software without restriction, including without limitation the rights
+%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%% copies of the Software, and to permit persons to whom the Software is
+%% furnished to do so, subject to the following conditions:
+%%
+%% The above copyright notice and this permission notice shall be included in
+%% all copies or substantial portions of the Software.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+%% THE SOFTWARE.
+%% -----------------------------------------------------------------------------
+-module(stack_props).
+-compile(export_all).
+
+-include_lib("proper/include/proper.hrl").
+-include_lib("hamcrest/include/hamcrest.hrl").
+
+prop_new_0_creates_an_empty_stack() ->
+    ?FORALL(S, stack:stack(any()),
+          ?assertThat(S, is_empty_stack())).
+
+is_empty_stack() ->
+    match_mfa(stack, empty, []).
